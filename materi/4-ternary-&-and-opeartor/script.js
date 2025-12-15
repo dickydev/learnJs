5e2f3f0527f0d26e1c7fd716b201433665784aa2
dickydev <dickyfernando062@gmail.com>
2025-12-15 15:51:11 +0700

    feat: 4-ternary-&-and-opeartor

--- materi/4-ternary-&-and-opeartor/script.js
+++ materi/4-ternary-&-and-opeartor/script.js
@@ -1,5 +1,40 @@
 /**
  * Ternary Operator
- * 
- * Syntax :
+ *
+ * Pattern :
+ * <condition> ? "true expression" : "false expression"
  */
+
+let trafficLight = "hijau";
+let banMeledak = true;
+
+trafficLight === "merah"
+  ? console.log("Stoppp donggss..")
+  : banMeledak === true
+  ? console.log("Stoppp donggss..")
+  : console.log("Jalann donggss..");
+
+if (trafficLight === "merah") {
+  console.log("Stoppp donggss..");
+} else {
+  console.log("Jalann dongsss");
+}
+
+// Nested (bercabang) -> If Statement
+
+/**
+ * AND Operator For Conditional:
+ *
+ * truthy && truthy -> yang muncul adalah Truthy Paling kanan
+ */
+
+let sudahMakan = true;
+let umur = 0;
+
+console.log(umur && sudahMakan);
+
+let isLogin = true;
+// isLogin && <Profile />;
+
+console.log(1 && true);
+console.log(true && 1);
